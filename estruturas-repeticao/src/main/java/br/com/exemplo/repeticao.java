2e5bd89f7b518f30b6e882a79b0c6b40bc462079
CrisleiKeli <crislei.jenuino@etec.sp.gov>
2025-09-30 13:31:13 -0300

    at java

---
+++ estruturas-repeticao/src/main/java/br/com/exemplo/repeticao.java
@@ -0,0 +1,11 @@
+package main.java.br.com.exemplo;
+
+public class repeticao {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Repetição número: " + i);
+        }
+    }
+    
+}
