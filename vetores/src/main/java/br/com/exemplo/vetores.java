cf3a8cce521e259ddf143d320368a989f37ba651
CrisleiKeli <crislei.jenuino@etec.sp.gov>
2025-09-30 16:24:32 -0300

    at vetiores java

---
+++ vetores/src/main/java/br/com/exemplo/vetores.java
@@ -0,0 +1,17 @@
+package main.java.br.com.exemplo;
+
+public class vetores {
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Elemento na posição " + i + ": " + numeros[i]);
+        }
+    }
+    
+}
